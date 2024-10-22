08eafa86c9b84a948124d29d1600553424b55841
kothahemanth <hemanthkotha17@gmail.com>
2024-10-22 19:37:13 +0530

    Json Format Done

--- srv/service.js
+++ srv/service.js
@@ -3,44 +3,143 @@ const cds = require('@sap/cds');
 module.exports = cds.service.impl(async function () {
     const purchaseapi = await cds.connect.to('CE_PURCHASEORDER_0001');
 
-    this.on('READ','PurchaseOrder', async (req) => {
-            const PurchaseOrderData = await purchaseapi.run(req.query);
-            return PurchaseOrderData;
-    });
+    this.on('READ', 'PurchaseOrder', async (req) => {
+        // Select only the fields defined in service.cds for PurchaseOrder
+        const purchaseOrders = await purchaseapi.run(
+            SELECT.from('PurchaseOrder')
+                .columns(
+                    'PurchaseOrder', 
+                    'PurchasingDocumentOrigin', 
+                    'PurchaseOrderDate', 
+                    'Language', 
+                    'CompanyCode', 
+                    'PurchasingOrganization', 
+                    'Supplier', 
+                    'DocumentCurrency', 
+                    'TaxReturnCountry', 
+                    'VATRegistrationCountry'
+                )
+        );
 
-    this.on('READ', 'PurchaseOrderItem', async (req) => {
-            const purchaseOrderItemData = await purchaseapi.run(req.query);
-            return purchaseOrderItemData;
-    }),
+        const enrichedPurchaseOrders = await Promise.all(
+            purchaseOrders.map(async (purchaseOrder) => {
+                const purchaseOrderItems = await purchaseapi.run(
+                    SELECT.from('PurchaseOrderItem')
+                        .columns(
+                            'PurchaseOrder', 
+                            'PurchaseOrderItem', 
+                            'PurchaseOrderCategory', 
+                            'DocumentCurrency', 
+                            'MaterialGroup', 
+                            'Material', 
+                            'MaterialType', 
+                            'ManufacturerMaterial', 
+                            'PurchaseOrderItemText', 
+                            'ProductTypeCode', 
+                            'CompanyCode', 
+                            'Plant', 
+                            'PurchaseOrderQuantityUnit', 
+                            'BaseUnit', 
+                            'OrderPriceUnit', 
+                            'NetAmount', 
+                            'GrossAmount', 
+                            'OrderQuantity', 
+                            'NetPriceAmount'
+                        )
+                        .where({ PurchaseOrder: purchaseOrder.PurchaseOrder })
+                );
 
-    this.on('READ', 'PurOrderItemPricingElement', async (req) => {
-            const PurOrderItemPricingElement = await purchaseapi.run(req.query);
-            return PurOrderItemPricingElement;
-    });
+                const enrichedItems = await Promise.all(
+                    purchaseOrderItems.map(async (item) => {
+                        const [notes, accountAssignments, pricingElements, itemNotes] = await Promise.all([
+                            purchaseapi.run(
+                                SELECT.from('PurchaseOrderNote')
+                                    .columns('PurchaseOrder', 'TextObjectType', 'Language', 'PlainLongText')
+                                    .where({ PurchaseOrder: item.PurchaseOrder })
+                            ),
+                            purchaseapi.run(
+                                SELECT.from('PurchaseOrderAccountAssignment')
+                                    .columns(
+                                        'PurchaseOrder', 
+                                        'PurchaseOrderItem', 
+                                        'AccountAssignmentNumber', 
+                                        'CostCenter', 
+                                        'OrderQuantityUnit', 
+                                        'Quantity', 
+                                        'MultipleAcctAssgmtDistrPercent', 
+                                        'DocumentCurrency', 
+                                        'IsDeleted', 
+                                        'GLAccount', 
+                                        'ControllingArea', 
+                                        'ProfitCenter', 
+                                        'FunctionalArea', 
+                                        'IsFinallyInvoiced', 
+                                        'SettlementReferenceDate', 
+                                        'EarmarkedFundsDocumentItem', 
+                                        'ValidityDate', 
+                                        'ChartOfAccounts', 
+                                        'CreationDate', 
+                                        'IsAcctLineFinal', 
+                                        'CompanyCode'
+                                    )
+                                    .where({ PurchaseOrder: item.PurchaseOrder, PurchaseOrderItem: item.PurchaseOrderItem })
+                            ),
+                            purchaseapi.run(
+                                SELECT.from('PurOrderItemPricingElement')
+                                    .columns(
+                                        'PurchaseOrder', 
+                                        'PurchaseOrderItem', 
+                                        'PricingDocument', 
+                                        'PricingDocumentItem', 
+                                        'PricingProcedureStep', 
+                                        'PricingProcedureCounter', 
+                                        'ConditionApplication', 
+                                        'ConditionType', 
+                                        'PriceConditionDeterminationDte', 
+                                        'ConditionCalculationType', 
+                                        'ConditionBaseAmount', 
+                                        'ConditionRateAmount', 
+                                        'ConditionBaseValue', 
+                                        'ConditionCurrency', 
+                                        'ConditionQuantity', 
+                                        'ConditionQuantityUnit', 
+                                        'ConditionCategory', 
+                                        'ConditionOrigin', 
+                                        'ConditionAmount', 
+                                        'TransactionCurrency', 
+                                        'ConditionControl', 
+                                        'ConditionClass', 
+                                        'ConditionTypeName', 
+                                        'ConditionBaseValueUnit', 
+                                        'ConditionRateValueIntlUnit', 
+                                        'ConditionRateValueUnit'
+                                    )
+                                    .where({ PurchaseOrder: item.PurchaseOrder, PurchaseOrderItem: item.PurchaseOrderItem })
+                            ),
+                            purchaseapi.run(
+                                SELECT.from('PurchaseOrderItemNote')
+                                    .columns('PurchaseOrder', 'PurchaseOrderItem', 'TextObjectType', 'Language', 'PlainLongText', 'PurchaseOrderItemUniqueID')
+                                    .where({ PurchaseOrder: item.PurchaseOrder, PurchaseOrderItem: item.PurchaseOrderItem })
+                            )
+                        ]);
 
-    this.on('READ', 'POSubcontractingComponent', async (req) => {
-            const POSubcontractingComponent = await purchaseapi.run(req.query);
-            return POSubcontractingComponent;
-    });
+                        return {
+                            ...item,
+                            PurchaseOrderNote: notes,
+                            PurchaseOrderAccountAssignment: accountAssignments,
+                            PurOrderItemPricingElement: pricingElements,
+                            PurchaseOrderItemNote: itemNotes
+                        };
+                    })
+                );
 
-    this.on('READ', 'PurchaseOrderScheduleLine', async (req) => {
-            const PurchaseOrderScheduleLine = await purchaseapi.run(req.query);
-            return PurchaseOrderScheduleLine;
-    });
+                return {
+                    ...purchaseOrder,
+                    PurchaseOrderItem: enrichedItems
+                };
+            })
+        );
 
-    this.on('READ', 'PurchaseOrderNote', async (req) => {
-            const PurchaseOrderNote = await purchaseapi.run(req.query);
-            return PurchaseOrderNote;
-    });
-
-    this.on('READ', 'PurchaseOrderAccountAssignment', async (req) => {
-            const PurchaseOrderAccountAssignment = await purchaseapi.run(req.query);
-            return PurchaseOrderAccountAssignment;
-    });
-
-    this.on('READ', 'PurchaseOrderItemNote', async (req) => {
-            const PurchaseOrderItemNote = await purchaseapi.run(req.query);
-            return PurchaseOrderItemNote;
+        return enrichedPurchaseOrders;
     });
 });
-    
